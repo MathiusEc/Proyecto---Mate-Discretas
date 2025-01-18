a8e3b9439a671c6903671fb1b4074afeeeacc463
Mathius <you@example.com>
2025-01-17 23:46:39 -0500

    Archivo C creado

---
+++ P35535Abet.c
@@ -0,0 +1,66 @@
+/*
+Pantallas que debe tener el programa
+1.	Inicio caratula
+MATEMATICAS DISCRETAS 1
+NOMBRES DE LOS INTEGRANTES
+DESCRIPCION DE LO QUE HACE EL PROGRAMA
+AÑO LECTIVO
+2.	Ingreso de número de variables lógicas
+Digite el número de variables lógicas, solo 2 o 3:  2
+Debe validarse que solo se digite los números dos o tres, caso contrario envié mensaje al usuario 
+
+3.	Ingreso de valores 
+Ingrese los valores de salida, 0 o 1 para cada fila de la tabla de verdad:  
+A     B    S
+-------------------
+[0] [0]   0
+[0] [1]   1
+[1] [0]   1
+[1] [1]   0
+Debe validarse que solo se digite 0 o 1
+4.	Generación de expresión booleana
+La expresión booleana generada es: A’*B + B’*A
+
+5.	Generación de LA TABLA DE VERDAD
+Tabla de verdad de la expresión A’*B + B’*A
+
+A’ * B      +     B’ * A
+-----------------------
+1   0   0    0      1  0  0 
+1   1   1    1      0  0  0
+0   0   0    1      1  1  1
+0   0   1    0      0  0  1
+Debe presentar los resultados parciales en color verde y el final en rojo
+
+*/
+
+#include <stdio.h>
+int main(){
+
+    printf("\n==============================\n"); 
+    printf(" MATEMATICAS DISCRETAS I\n"); 
+    printf("\n==============================\n"); 
+    printf(" NOMBRES DE LOS INTEGRANTES\n"); 
+    printf("\n==============================\n"); 
+    printf(" DESCRIPCION DE LO QUE HACE EL PROGRAMA\n"); 
+    printf("\n==============================\n"); 
+    printf(" AÑO LECTIVO\n"); 
+    printf("\n==============================\n");
+
+// ya voy a buscar como imprimirle con colores y medio bastante decorando
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
